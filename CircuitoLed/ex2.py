cfc8e339d13cf20866183d58e5db4db9474284bd
leorr <leonardoribeiro.2016@alunos.utfpr.edu.br>
2019-04-25 16:38:42 -0300

    2019/04/25 16:38:42 need to figure out chi-square

--- CircuitoLed/ex2.py
+++ CircuitoLed/ex2.py
@@ -12,12 +12,60 @@
 #   devemos então estimar R com essas informações
 #   no nosso caso R é suposto como desconhecido
 #   então       vd(R)=i*(R) + v
-
 import matplotlib.pyplot as plt
 import csv
 import numpy as np
 from scipy import stats
 
+
+def pearsonregression(x,y):
+    r= np.polyfit(x,y,1,full=True)          #regressao linear
+    ye=[]                                   #y esperado
+    for i in range(0,np.size(x),1):             
+        ye.append(r[0][0] * x[i] + r[0][1])
+    ye=np.array(ye)
+    ym=0                                    #y medio
+    for i in range(0,np.size(y),1):             
+        ym=ym+y[i]
+    ym=ym/np.size(y)
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+
+    ax.plot(x,y,'.',label='pontos obtidos',color='red')
+    ax.plot(x,ye,label='linha de regressão')
+
+    for i in range(0,np.size(x),1):                     #
+        res = [x[i],x[i]],[y[i],ye[i]]                #resíduos, x -> xe = x E y -> ye  
+        ax.plot(res[0],res[1],'--',linewidth=0.5,color='red')     #
+    ax.plot(0,0,'--',linewidth=0.5,color='red',label='barras de erro(resíduos)')
+
+    sres=0
+    for i in range(0,np.size(y),1):
+        sres=sres+  ((y[i]-ye[i])**2)   
+    sm=0
+    for i in range(0,np.size(y),1):
+        sm=sm+((y[i]-ym)**2)
+    R2=1-sres/sm
+    
+    
+    c2='(todo)χ²=' + str("{:.3f}".format(00000))            #convertendo chi quadrado para ser mostrado em texto
+    R2='R²='+str("{:.2f}".format(R2))#coeficiente de correlação² em texto
+    eq='y='+str("{:.2f}".format(r[0][0])) + 'x+' + str("{:.2f}".format(r[0][1]))#equaçao da reta tem texto
+
+
+    plt.axhline(0, color='black')               #
+    plt.axvline(0, color='black')               #desenhando as origens
+    ax.text(max(x)+1,3.5,c2)
+    ax.text(max(x)+1,4,R2)
+    ax.text(max(x)+1,4.5,eq)
+    ax.set_aspect(1)
+    plt.legend()
+    plt.xlim(-1,4.85)
+    plt.ylim(-1,4.85)
+    plt.grid(which='both')
+    plt.show()
+#importando os dados
 vd=[]
 i=[]
 v=[]
@@ -35,47 +83,6 @@ v = np.array(v)
 
 y=i#y observado
 x=vd-v
-
-r= np.polyfit(x,y,1,full=True)    #regressao linear
-ry=np.polyval(r[0],x)             #a regressao si é r[0]
-
-
-fig = plt.figure()
-ax = fig.add_subplot(111)
-ranger =np.arange(min(x)-3,max(x)+3.1,0.01) #elaborando um range para exibição 
-plt.axhline(0, color='black')               #
-plt.axvline(0, color='black')               #desenhando as origens
-
-ax.plot(x,y,'.',label='pontos obtidos',color='red')
-ax.plot(ranger,np.polyval(r[0],ranger),label='linha de regressão')
-
-for i in range(0,np.size(x),1):             #
-    duple = [x[i],x[i]],[y[i],ry[i]]        #resíduos
-    ax.plot(duple[0],duple[1],color='red')  #
-ax.plot(0,0,color='red',label='barras de erro(resíduos)')
-
-
-#y esperado
-ye=[]
-for i in range(0,np.size(x),1):             
-    ye.append(r[0][0] * x[i] + r[0][1])
-
-chi2=stats.chisquare(y,ye,8)[0] #chi quadrado para 8 graus de liberdade
-
-c2='χ²=' + str(chi2)            #convertendo chi quadrado para ser mostrado em texto
-R2='R²='+str("{:.2f}".format(stats.linregress(x, y)[2] ** 2))#coeficiente de correlação² em texto
-eq='y='+str("{:.2f}".format(r[0][0])) + 'x+' + str("{:.2f}".format(r[0][1]))#equaçao da reta tem texto
-
-ax.text(5.5,3.5,c2)
-ax.text(5.5,4,R2)
-ax.text(5.5,4.5,eq)
-ax.set_aspect(1)
-plt.legend()
-plt.xlim(-2,max(max(x),max(y))+0.5)
-plt.ylim(-2,max(max(x),max(y))+0.5)
-plt.grid(which='both')
-plt.show()
-#primeira parte pronta!
-#agora falta realizar p y=i x=vd
-
-
+pearsonregression(x,y)
+x=v
+pearsonregression(x,y)
