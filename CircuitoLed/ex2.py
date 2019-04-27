f6dcb2e7ce0f79614c487de904f002d3e6d73dd7
leorr <leonardoribeiro.2016@alunos.utfpr.edu.br>
2019-04-27 10:24:43 -0300

    2019/04/27 10:24:43

--- CircuitoLed/ex2.py
+++ CircuitoLed/ex2.py
@@ -18,25 +18,33 @@ import numpy as np
 from scipy import stats
 
 
-def pearsonregression(x,y):
-    r= np.polyfit(x,y,1,full=True)          #regressao linear
+def lineregression(x,y):
     n = np.size(y)                          #tamanho amostral
 
-    ye=[]                                   #y esperado
-    for i in range(0,n,1):             
-        ye.append(r[0][0] * x[i] + r[0][1])
-    ye=np.array(ye)
-
     ym=0                                    #y medio
-    for i in range(0,n,1):             
+    for i in range(0,n):             
         ym=ym+y[i]
     ym=ym/n
     
     xm=0                                    #x medio
-    for i in range(0,n,1):             
+    for i in range(0,n):             
         xm=xm+x[i]
-    xm=xm/n
+    xm=xm/n                                 
+                                            
+    sxx=0                                   #hipotese(metodo dos mínimos quadrados)
+    sxy=0                                   #y = ax + b
+    for i in range(0,n):                  #a = sxy/sxx
+        sxx=(x[i]-xm)**2 + sxx              #b = ym - a * xm
+
+    for i in range(0,n):
+        sxy=(x[i]-xm)*(y[i]-ym) + sxy
+    a=sxy/sxx
+    b=ym-(a*xm)
     
+    ye=[]
+    for i in range(0,n):
+        ye.append(a*x[i]+b)
+
 
     fig = plt.figure()                      
     ax = fig.add_subplot(111)               #preparando plots
@@ -44,39 +52,46 @@ def pearsonregression(x,y):
     ax.plot(x,y,'.',label='pontos obtidos',color='red')
     ax.plot(x,ye,label='linha de regressão')
 
-    for i in range(0,n,1):                  #
+    for i in range(0,n):                  #
         res = [x[i],x[i]],[y[i],ye[i]]      #resíduos, xo E yo -> ye  
         ax.plot(res[0],res[1],'--',linewidth=0.5,color='red')     #
-    ax.plot(0,0,'--',linewidth=0.5,color='red',label='barras de erro(resíduos)')
+    ax.plot(0,0,'--',linewidth=0.5,color='red',label='resíduos')
+    plt.errorbar(x,y,0.005,0.0525,fmt='.k',alpha=0.5,label='barra de erros')
 
-    #temos a regressão da reta, b0 e b1 portanto vamos aos testes:
     SQres = 0
-    for i in range(0,n,1):
+    for i in range(0,n):
         SQres= (y[i]-ye[i])**2 + SQres
 
     SQexp = 0
-    for i in range(0,n,1):
+    for i in range(0,n):
         SQexp = (ye[i]-ym)**2 + SQexp
 
     SQtot = SQexp + SQres 
     R2=SQexp/SQtot
+    #chisq=sum((y[i]-ye[i])/si)**2
+    stdm=np.std(y)/np.sqrt(n)
 
-    c2='χ²=' + str("{:.3f}".format(stats.chisquare(x,y)[0]))            #convertendo chi quadrado para ser mostrado em texto
-    R2='R²='+str("{:.2f}".format(R2))#coeficiente de correlação² em texto
-    eq='y='+str("{:.2f}".format(r[0][0])) + 'x+' + str("{:.2f}".format(r[0][1]))#equaçao da reta tem texto
+    chisq=0
+    for i in range(0,n,1):
+        chisq=(((y[i]-ye[i])/stdm)**2) + chisq
 
+    c2='χ²=' + str("{:.3f}".format(chisq))           #convertendo chi quadrado para ser mostrado em texto
+    R2='R²='+str("{:.3f}".format(R2))#coeficiente de correlação² em texto
+    eq='y='+str("{:.2f}".format(a)) + 'x' + str("{:.2f}".format(b))#equaçao da reta tem texto
 
     plt.axhline(0, color='black')               #
     plt.axvline(0, color='black')               #desenhando as origens
-    ax.text(max(x)+1,3.5,c2)
-    ax.text(max(x)+1,4,R2)
-    ax.text(max(x)+1,4.5,eq)
+    ax.text(5,3.5,c2)
+    ax.text(5,4,R2)
+    ax.text(5,4.5,eq)
     ax.set_aspect(1)
     plt.legend()
     plt.xlim(-1,4.85)
     plt.ylim(-1,4.85)
     plt.grid(which='both')
     plt.show()
+#FIM DA FUNÇÃO
+
 #importando os dados
 vd=[]
 i=[]
@@ -93,8 +108,8 @@ vd = np.array(vd)
 i = np.array(i)
 v = np.array(v)
 
-y=i#y observado
+y=i
 x=vd-v
-pearsonregression(x,y)
+lineregression(x,y)
 x=v
-pearsonregression(x,y)
+lineregression(x,y)
